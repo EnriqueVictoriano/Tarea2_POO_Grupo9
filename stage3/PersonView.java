1266355f6db4bdc10cf16d107b5e6cbe92957eeb
Josschua <66189800+Josschua@users.noreply.github.com>
2023-05-14 14:15:57 -0400

    Add files via upload

--- stage3/PersonView.java
+++ stage3/PersonView.java
@@ -1,2 +1,29 @@
-public class PersonView {
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+
+public class PersonView extends Circle {
+    public PersonView(Person persona, Color color, double radius) {
+        super(persona.getX(), persona.getY(), radius, color);
+        this.persona = persona;
+        this.color = color;
+        this.radius = radius;
+    }
+
+    public void updatePosition() {
+        setCenterX(persona.getX());
+        setCenterY(persona.getY());
+    }
+
+    public void updateColor() {
+        if (persona.getdetect()) {
+            setFill(Color.RED);
+        } else {
+            setFill(color);
+        }
+    }
+
+    private Person persona;
+    private Color color;
+    private double radius;
 }
+
