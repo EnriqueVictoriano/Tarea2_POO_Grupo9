113e1be2cb3b0cfab2afad0cfba4578399c5c602
EnriqueVictoriano <65976413+EnriqueVictoriano@users.noreply.github.com>
2023-05-14 19:36:51 -0400

    Update Person.java

--- stage3/Person.java
+++ stage3/Person.java
@@ -5,22 +5,38 @@ public class Person {
         this.y = y;
         this.isDetected = false;
     }
+
+    public void inMove(int x1, int y1) {
+        this.x = x1;
+        this.y = y1;
+    }
+
     public void detect() {
         this.isDetected = true;
     }
-    public boolean getdetect(){
+
+    public boolean getdetect() {
         return isDetected;
     }
-    public int getX(){
+
+    public int getX() {
         return x;
     }
-    public  int getY(){
+
+    public int getY() {
         return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     private int id;
     private int x;
     private int y;
     private boolean isDetected;
-    .
-        <.
+}
